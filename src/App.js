61cd57eb442ebf0aaf41dc5a82f5ffd48f9e0274
clarabrandt <mariaclarabrandt@gmail.com>
2019-02-13 12:04:16 +0100

    fixing url bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout.js';
 import Login from './components/Login.js';
@@ -9,9 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
+        <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
         <Switch>
           <Route path="/login" component={Login} /> 
-          <Route exact path='/ccbpigeons/' component={Layout}/>
+          <Route exact path='/' component={Layout}/>
           <Route path='/home' component={Layout}/>
           <Route path="/sobre" component={Layout} /> 
           <Route path="/resultados" component={Layout} /> 
