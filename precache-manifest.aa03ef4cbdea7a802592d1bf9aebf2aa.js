2e15bb9b1f933a5c4f04711c53c355c966402354
clarabrandt <mariaclarabrandt@gmail.com>
2019-05-01 11:34:48 +0200

    Updates

---
+++ precache-manifest.aa03ef4cbdea7a802592d1bf9aebf2aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6128a8abb11641f1abf506d396d2b8ff",
+    "url": "/static/media/pombo-correio.6128a8ab.jpg"
+  },
+  {
+    "revision": "5a2aa573e07572905a27c3b2c530c638",
+    "url": "/static/media/ccbpombinho.5a2aa573.jpg"
+  },
+  {
+    "revision": "790fb9f2ba8c6e74a640ed431a9b5751",
+    "url": "/static/media/Overview_pombal2.790fb9f2.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7574805eb0a0f7f73726",
+    "url": "/static/js/main.7574805e.chunk.js"
+  },
+  {
+    "revision": "aec673f944f1b3f64fb0",
+    "url": "/static/js/1.aec673f9.chunk.js"
+  },
+  {
+    "revision": "7574805eb0a0f7f73726",
+    "url": "/static/css/main.1aa545b7.chunk.css"
+  },
+  {
+    "revision": "6321727ae480760f5fb609a0a6a9586f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ccbpigeons/precache-manifest.cb1d577e39b206c54d9eef2fd6c25293.js"
+  "/precache-manifest.aa03ef4cbdea7a802592d1bf9aebf2aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ccbpigeons/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
