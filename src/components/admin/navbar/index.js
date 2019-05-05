a66bd8aabbf6fa63750206532851b675e0ac8fc6
clarabrandt <40141410+clarabrandt@users.noreply.github.com>
2019-05-05 12:04:02 +0200

    Revert "admin menu working with material ui"

--- src/components/admin/navbar/index.js
+++ src/components/admin/navbar/index.js
@@ -1,63 +1,62 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { compose } from "recompose";
-import { withFirebase } from "../../firebase";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { withFirebase } from '../../firebase';
 import withRoot from "../../../withRoot";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-
-import classNames from "classnames";
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import classNames from 'classnames';
 
 const drawerWidth = 240;
 
 const styles = theme => ({
   toolbar: {
-    paddingRight: 24 // keep right padding when drawer closed
+    paddingRight: 24, // keep right padding when drawer closed
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginLeft: 12,
-    marginRight: 36
+    marginRight: 36,
   },
   menuButtonHidden: {
-    display: "none"
+    display: 'none',
   },
   title: {
-    flexGrow: 1
-  }
+    flexGrow: 1,
+  },
 });
 
 class Navbar extends React.Component {
+
   signOut = () => {
     this.props.firebase.auth.signOut();
-  };
+  }
   render() {
     const { classes } = this.props;
     return (
       <AppBar
         position="absolute"
-        className={classNames(
-          classes.appBar,
-          this.props.open && classes.appBarShift
-        )}
+        className={classNames(classes.appBar, this.props.open && classes.appBarShift)}
       >
         <Toolbar disableGutters={!this.props.open} className={classes.toolbar}>
           <IconButton
@@ -66,7 +65,7 @@ class Navbar extends React.Component {
             onClick={this.props.handleDrawerOpen}
             className={classNames(
               classes.menuButton,
-              this.props.open && classes.menuButtonHidden
+              this.props.open && classes.menuButtonHidden,
             )}
           >
             <MenuIcon />
@@ -79,7 +78,12 @@ class Navbar extends React.Component {
             className={classes.title}
           >
             CCBPigeons Admin
-          </Typography>
+              </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
         </Toolbar>
       </AppBar>
     );
@@ -88,7 +92,7 @@ class Navbar extends React.Component {
 
 const NavbarComponent = compose(
   withRouter,
-  withFirebase
+  withFirebase,
 )(withRoot(withStyles(styles)(Navbar)));
 
 export default Navbar;
